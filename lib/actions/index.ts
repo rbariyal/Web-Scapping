6970b66831dccc1fdb0a682cde98c3b1a26feb69
CDAC <cdac@CDAC09s-iMac.local>
2024-01-10 15:20:14 +0530

    error fixed

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,114 +5,112 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
-import { Console } from "console";
-import { generateEmailBody, sendEmail } from "../nodemailer";
 import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if(!productUrl) return;
 
-
- 
-export async function scrapeAndStoreProduct(productUrl:string)
-{
-if(!productUrl) return;
-
-try {
+  try {
     connectToDB();
-    const scrapedProduct=await scrapeAmazonProduct(productUrl)
+
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
     if(!scrapedProduct) return;
 
-let product=scrapedProduct;
+    let product = scrapedProduct;
 
-const existingProduct=await Product.findOne({url:scrapedProduct.url})
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-
-
-if(existingProduct)
-{
-    const updatedPriceHistory:any=[
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
         ...existingProduct.priceHistory,
-        {price:scrapedProduct.currentPrice}
-    ]
+        { price: scrapedProduct.currentPrice }
+      ]
 
-    product={
+      product = {
         ...scrapedProduct,
-        priceHistory:updatedPriceHistory,
-        lowestPrice:getLowestPrice(updatedPriceHistory),
-        highestPrice:getHighestPrice(updatedPriceHistory),
-        averagePrice:getAveragePrice(updatedPriceHistory)
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
     }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
 }
 
-const newProduct=await Product.findOneAndUpdate({url:scrapedProduct.url},
-product,
-{upsert:true,new:true}
-)
-revalidatePath(`/product/${newProduct._id}`)
-} catch (error:any) {
-    throw new Error(`${error.message}`)
-}
-}
-
-export async function getProductById(productId:string)
-{
-try {
+export async function getProductById(productId: string) {
+  try {
     connectToDB();
-    const product=await Product.findOne({_id:productId});
+
+    const product = await Product.findOne({ _id: productId });
+
+    if(!product) return null;
+
     return product;
-} catch (error) {
-console.log(error)    
-}
-}
-
-export async function getAllProducts()
-{
-    try {
-        connectToDB();
-        const products=await Product.find();
-        console.log(products)
-        return products;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export async function getSimilarProducts(productId:string)
-{
-    try {
-        connectToDB();
-        const currentPrice=await Product.findById(productId);
-        if(!currentPrice) return null;
-     const similarProducts=await Product.find(
-        {
-            _id:{$ne:productId},
-        }
-     ).limit(3)
-     return similarProducts;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export async function addUserEmailToProduct(productId:string,userEmail:string)
-{
-try {
-    const product=await Product.findById(productId);
-    if(!product) return
-
-    const userExists=product.users.some((user:User)=>
-    user.email===userEmail);
-
-    if(!userExists)
-    {
-        product.users.push({email:userEmail});
-        await product.save();
-        const emailContent=await generateEmailBody(product,"WELCOME");
-        await sendEmail(emailContent,[userEmail])
-    }
-
-
-  
-} catch (error) {
+  } catch (error) {
     console.log(error);
+  }
 }
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+  try {
+    const product = await Product.findById(productId);
+
+    if(!product) return;
+
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if(!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
